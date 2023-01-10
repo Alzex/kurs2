7f998d7cf903aa47617f3a35b8b5354086641349
gonnagetbetter <denissss906@gmail.com>
2023-01-11 01:04:36 +0200

    added group methods

--- src/class/core/group.js
+++ src/class/core/group.js
@@ -7,7 +7,19 @@ const GroupCollection = new Collection();
 class Group extends Entity {
   constructor(name, collection = GroupCollection, type = 'Group') {
     super(name, collection, type);
+    this.members = [];
+  }
 
+  async addUser(tgID) {
+    await this.members.push(tgID);
+  }
+
+  async deleteUser(tgID) {
+    await this.members.splice(this.members.indexOf(tgID), 1);
+  }
+
+  async splitTheBill(price) {
+    await return (price / this.members.length)
   }
 }
 
