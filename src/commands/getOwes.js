52fc94992ca912cdfe86c76faa557f11a4476514
gonnagetbetter <denissss906@gmail.com>
2023-01-11 14:28:13 +0200

    Added getOwes

---
+++ src/commands/getOwes.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const Command = require('../class/core/command');
+
+const getOwes = async (ctx, db, [name]) => {
+  const currentGroup = await db.groupCollection.findByID(name);
+  if (ctx.message.from.id === currentGroup.ownerId) {
+    const result = [];
+    for (const memberID in currentGroup.members) {
+      const member = db.userCollection.findByID(memberID);
+      const name = member.name;
+      const owe = member.owes.get(currentGroup.name);
+      result.push({ name, owe });
+    }
+    return result;
+  } else return 'You aren\'t group leader';
+};
+
+module.exports = new Command('get_owes', 'Shows owes in current group', getOwes);
