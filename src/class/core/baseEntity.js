251ab3a440f4015a4fefd68854af4ca8756dc53b
gonnagetbetter <denissss906@gmail.com>
2023-01-11 20:19:53 +0200

    fixed baseEntity

--- src/class/core/baseEntity.js
+++ src/class/core/baseEntity.js
@@ -6,25 +6,23 @@ class Entity {
 
   constructor(name, db, type) {
     this.name = name;
-    this.collection = new Collection(db, type);
     this.type = type;
-    this.db = db;
   }
 
-  async create(name) {
-    const entity = await this.collection.ifFieldExist(name);
+  async create(name, collection) {
+    const entity = await collection.ifFieldExist(name);
     if (!entity) {
-      await this.collection.create(new Entity(name, this.collection,
+      await this.collection.create(new Entity(name, collection,
         this.type));
       const result = this.type + 'has been created.';
       return result;
     } else return 'This name is already taken.';
   }
 
-  async delete(name) {
-    const entity = await this.collection.ifFieldExist(name);
+  async delete(name, collection) {
+    const entity = await collection.ifFieldExist(name);
     if (entity) {
-      await this.collection.delete(name);
+      await collection.delete(name);
       const result = this.type + 'has been deleted.';
       return result;
     } else return 'This name does not exist.';
