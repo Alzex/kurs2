61ec579c859f849673baf46cbb0d606a53607b0f
gonnagetbetter <denissss906@gmail.com>
2023-01-14 00:40:02 +0200

    fixed

--- src/commands/getMyOwe.js
+++ src/commands/getMyOwe.js
@@ -1,13 +1,15 @@
 'use strict';
 
 const Command = require('../class/core/command');
-const User = require('../class/core/user');
 
-const getMyOwe = async (ctx, { userCollection }, name) => {
-  const tgId = ctx.message.from.id;
-  const userData = await userCollection.findOneById(tgId);
-  console.log(userData.owe[name]);
-  return await ctx.sendMessage('Your debt in group ' + userData.owe[name]);
+const getMyOwe = async (ctx, { userCollection }, [ name ]) => {
+  console.log(name);
+  if (name) {
+    const tgId = ctx.message.from.id;
+    const userData = await userCollection.findOneById(tgId);
+    console.log(userData.owe[name]);
+    return await ctx.sendMessage('Your debt in group is ' + userData.owe[name]);
+  } return await ctx.sendMessage('Specify group name');
 };
 
 module.exports = new Command('get_my_owe', 'Shows your owe in group', getMyOwe);
