56ba532435e969c5520603c32a94304bd8b991a4
gonnagetbetter <denissss906@gmail.com>
2023-01-15 17:15:59 +0200

    added some code

--- src/commands/leavegroup.js
+++ src/commands/leavegroup.js
@@ -4,10 +4,14 @@ const Command = require('../class/core/command');
 const Group = require('../class/core/group');
 const User = require('../class/core/user');
 
-const leave = (ctx, { groupCollection, userCollection }, [groupName]) => {
+const leave = async (ctx, { groupCollection, userCollection }, [groupName]) => {
     const userId = ctx.message.from.id;
-    const currentGroup = groupCollection.
-
+    const currentGroup = groupCollection.findOneByName(groupName);
+    if (!currentGroup.members.includes(userId)) {
+        await ctx.sendMessage('You are not member of this group')
+    }
+    const newMembers = 
+    const currentUser = userCollection.findOneById(userId); 
 }
 
 module.exports = new Command(
