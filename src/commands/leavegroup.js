d9c840bf1054f29fa8a2998a40248116a81dcb58
Alzex <alexzemly@gmail.com>
2023-01-17 02:22:41 +0200

    Linted

--- src/commands/leavegroup.js
+++ src/commands/leavegroup.js
@@ -1,20 +1,31 @@
 'use strict';
 
 const Command = require('../class/core/command');
-const Group = require('../class/core/group');
-const User = require('../class/core/user');
 
 const leave = async (ctx, { groupCollection, userCollection }, [groupName]) => {
-    const userId = ctx.message.from.id;
-    const groupRaw = await groupCollection.findOneById(groupName);
-    if (!groupRaw.members.includes(userId)) {
-        await ctx.sendMessage('You are not member of this group')
-    }
-    
-}
+  if (!groupName) {
+    await ctx.sendMessage('You must specify a group name');
+    return;
+  }
+  const userId = ctx.message.from.id;
+  const currentGroup = await groupCollection.findOneByName(groupName);
+
+  if (!currentGroup.members.includes(userId)) {
+    await ctx.sendMessage('You are not member of this group');
+    return;
+  }
+
+  currentGroup.members = currentGroup.members.filter((member) => member !== userId);
+
+  await Promise.all([
+    groupCollection.updateField(groupName, 'members', currentGroup.members),
+    ctx.sendMessage(`You left the group ${groupName}`)
+  ]);
+
+};
 
 module.exports = new Command(
-    'leave',
-    'Deleting you from group',
-    leave
-  );
+  'leave',
+  'Deleting you from group',
+  leave
+);
