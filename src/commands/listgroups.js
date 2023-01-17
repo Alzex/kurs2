2c98d547683975b4f5126a80f28b0fd25b836586
Alzex <alexzemly@gmail.com>
2023-01-17 10:07:08 +0200

    Fix

--- src/commands/listgroups.js
+++ src/commands/listgroups.js
@@ -4,13 +4,22 @@ const Command = require('../class/core/command');
 
 const listGroups = async (ctx, { groupCollection }) => {
   const ownerId = ctx.message.from.id;
-  const groups = await groupCollection.findByUserId(ownerId);
-  if (groups.length === 0) {
+  const [groups, groupsAsOwner] =
+    await Promise.all([
+      groupCollection.findByUserId(ownerId),
+      groupCollection.find({ ownerId })
+    ]);
+
+  if (!groups.length) {
     await ctx.sendMessage('You have no groups');
     return;
   }
   const groupsNames = groups.map((group) => group.name).join('\n');
-  await ctx.sendMessage(`Your groups:\n${groupsNames}`);
+  const groupsAsOwnerNames = groupsAsOwner.map((group) => group.name)
+    .join('\n');
+
+  await ctx.sendMessage(`Your groups:\n${groupsNames}\n
+  \nGroups you are owner of:\n${groupsAsOwnerNames}`);
 };
 
 module.exports = new Command('list_groups', 'Shows you groups where you ' +
