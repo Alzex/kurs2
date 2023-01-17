dca40de60a9823eac29052f3fdb1e30f6c8d6fa6
Alzex <alexzemly@gmail.com>
2023-01-17 09:50:09 +0200

    Fixed leave command

--- src/commands/leavegroup.js
+++ src/commands/leavegroup.js
@@ -2,7 +2,7 @@
 
 const Command = require('../class/core/command');
 
-const leave = async (ctx, { groupCollection, userCollection }, [groupName]) => {
+const leave = async (ctx, { groupCollection }, [groupName]) => {
   if (!groupName) {
     await ctx.sendMessage('You must specify a group name');
     return;
@@ -10,12 +10,19 @@ const leave = async (ctx, { groupCollection, userCollection }, [groupName]) => {
   const userId = ctx.message.from.id;
   const currentGroup = await groupCollection.findOneByName(groupName);
 
+  if (currentGroup.ownerId === userId) {
+    await ctx.sendMessage('You are the owner of this group. You cannot leave it.\n' +
+      'If you want to delete this group, use /delete_group <group_name> command.');
+    return;
+  }
+
   if (!currentGroup.members.includes(userId)) {
     await ctx.sendMessage('You are not member of this group');
     return;
   }
 
-  currentGroup.members = currentGroup.members.filter((member) => member !== userId);
+  currentGroup.members =
+    currentGroup.members.filter((member) => member !== userId);
 
   await Promise.all([
     groupCollection.updateField(groupName, 'members', currentGroup.members),
