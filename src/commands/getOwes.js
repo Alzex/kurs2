e27148235efa372a06e93267cb4aec33fa761c38
Alzex <alexzemly@gmail.com>
2023-01-17 10:40:29 +0200

    Fix

--- src/commands/getOwes.js
+++ src/commands/getOwes.js
@@ -3,16 +3,32 @@
 const Command = require('../class/core/command');
 
 const getOwes = async (ctx, { userCollection, groupCollection }, [name]) => {
+  if (!name) {
+    await ctx.sendMessage('You must specify a group name');
+    return;
+  }
   const currentGroup = await groupCollection.findOneByName(name);
+
+  if (!currentGroup) {
+    await ctx.sendMessage('There is no such group');
+    return;
+  }
+
   if (ctx.message.from.id === currentGroup.ownerId) {
     const result = [];
-    for (const memberID in currentGroup.members) {
-      const member = userCollection.findByID(memberID);
+    for (const memberID of currentGroup.members) {
+      const member = await userCollection.findOneById(memberID);
+      if (!member) {
+        continue;
+      }
       const name = member.name;
-      const owe = member.owes.get(currentGroup.name);
+      const owe = member.owe[currentGroup.name];
       result.push({ name, owe });
     }
-    await ctx.sendMessage(result);
+    await ctx.sendMessage(`Debts in group ${currentGroup.name}:\n
+    ${result.map((member) => `${member.name} owes ${member.owe || 0}\n`)
+    .join('')}`);
+    return;
   }
   await ctx.sendMessage('You are not the owner of this group.');
 };
